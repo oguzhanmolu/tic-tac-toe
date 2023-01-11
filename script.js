7bd89c701460f9bb3cc839e1fc15b726edd6d27e
Oguzhanmolu <oguzhanmolu@gmail.com>
2023-01-11 17:30:43 +0300

    minor css changes

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 'use strict';
+const title = document.querySelector('#title');
+const field = document.querySelectorAll('.field');
+const btnRestart = document.querySelector('#restart');
+
+field.forEach((ele) =>
+  ele.addEventListener('click', (e) => {
+    console.log((e.target.textContent = 'o'));
+  })
+);
