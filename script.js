4fd609dab56406f505f040e14024d9196ed79028
Oguzhanmolu <oguzhanmolu@gmail.com>
2023-01-14 16:51:17 +0300

    added basic logic

--- script.js
+++ script.js
@@ -1,10 +1,43 @@
 'use strict';
-const title = document.querySelector('#title');
-const field = document.querySelectorAll('.field');
-const btnRestart = document.querySelector('#restart');
+const Gameboard = (() => {
+  const playerFactory = (sign) => {
+    return { sign };
+  };
+  const playerX = playerFactory('X');
+  const playerO = playerFactory('O');
+  let gameboard = ['', '', '', '', '', '', '', '', ''];
+  const titleElement = document.querySelector('#title');
+  const fieldElements = document.querySelectorAll('.field');
+  const btnRestart = document.querySelector('#restart');
 
-field.forEach((ele) =>
-  ele.addEventListener('click', (e) => {
-    console.log((e.target.textContent = 'o'));
-  })
-);
+  fieldElements.forEach((ele) =>
+    ele.addEventListener('click', (e) => {
+      if (gameController.round >= 10) return;
+      let index = Number(e.target.dataset.index);
+
+      if (gameboard[index] === '') {
+        if (gameController.round % 2 !== 0) {
+          e.target.textContent = 'X';
+          titleElement.textContent === `Player O's turn`;
+          gameboard[index] === playerX.sign;
+          gameController.round++;
+          console.log(gameboard[index], gameController.round);
+        } else {
+          e.target.textContent = 'O';
+          titleElement.textContent === `Player X's turn`;
+          gameboard[index] === playerO.sign;
+          gameController.round++;
+          console.log(gameboard[index], gameController.round);
+        }
+      }
+    })
+  );
+  return { gameboard };
+})();
+
+const gameController = (() => {
+  let round = 1;
+  let gameFinished = false;
+
+  return { round };
+})();
