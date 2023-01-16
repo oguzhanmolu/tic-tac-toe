0c10f506ff48056d45d66a5f26e4c1570e0b2553
Aka <aka@pop-os.localdomain>
2023-01-16 19:16:46 +0300

    added a lazy restart function

--- script.js
+++ script.js
@@ -11,12 +11,13 @@ const Gameboard = (() => {
   const fieldElements = document.querySelectorAll('.field');
   const btnRestart = document.querySelector('#restart');
 
-  // This adds the player marks on field clicks.
+  // All occuring events when fields are clicked.
   fieldElements.forEach((ele) =>
     ele.addEventListener('click', (e) => {
       if (gameController.round >= 10 || gameboard[e.target.id] !== '') return;
       gameController.round++;
 
+      // This adds the player marks on field clicks.
       if (gameController.round % 2 == 0) {
         gameboard[e.target.id] = playerX.sign;
         e.target.textContent = playerX.sign;
@@ -29,6 +30,11 @@ const Gameboard = (() => {
       console.log(gameboard);
     })
   );
+
+  // Restart the game (I could reset every variable, but we don't need any ongoing value for game to continue. So this is way cleaner IMHO)
+  btnRestart.addEventListener('click', () => {
+    window.location.reload();
+  });
 })();
 
 // Everything about game controls
